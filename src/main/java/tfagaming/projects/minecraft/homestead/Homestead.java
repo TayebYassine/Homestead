22c5f56081421e9d19a989a170c68f08ad42ce54
TayebYassine <tayeb.fekihahmed@outlook.com>
2026-04-27 21:56:01 +0100

    updated plugin version (alpha)

--- src/main/java/tfagaming/projects/minecraft/homestead/Homestead.java
+++ src/main/java/tfagaming/projects/minecraft/homestead/Homestead.java
@@ -53,8 +53,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Homestead extends JavaPlugin {
-	private final static String VERSION = "5.1.0.1";
-	private final static boolean SNAPSHOT = false;
+	private final static String VERSION = "5.2.0.0-26w18a";
+	private final static boolean SNAPSHOT = true;
 	public static Database database;
 
 	// Cache
