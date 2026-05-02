70bb2d19067a9489361fbe2605a80a494797a889
TayebYassine <tayeb.fekihahmed@outlook.com>
2026-05-02 14:27:49 +0100

    feat: added sunrise and night time, changed sunset time cycle

--- src/main/java/tfagaming/projects/minecraft/homestead/weatherandtime/TimeType.java
+++ src/main/java/tfagaming/projects/minecraft/homestead/weatherandtime/TimeType.java
@@ -4,26 +4,30 @@ import java.util.List;
 
 public final class TimeType {
 	public final static int SERVER = 0;
-	public final static int MORNING = 1;
+	public final static int SUNRISE = 23500;
+	public final static int MORNING = 1000;
 	public final static int NOON = 6000;
 	public final static int AFTERNOON = 9000;
-	public final static int SUNSET = 12000;
-	public final static int NIGHT = 18000;
+	public final static int SUNSET = 13000;
+	public final static int NIGHT = 15000;
+	public final static int MIDNIGHT = 18000;
 	private TimeType() {
 	}
 
 	public static List<String> getAll() {
-		return List.of("server", "morning", "noon", "afternoon", "sunset", "night");
+		return List.of("server", "sunrise", "morning", "noon", "afternoon", "sunset", "night", "midnight");
 	}
 
 	public static int next(int current) {
 		return switch (current) {
-			case SERVER -> MORNING;
+			case SERVER -> SUNRISE;
+			case SUNRISE -> MORNING;
 			case MORNING -> NOON;
 			case NOON -> AFTERNOON;
 			case AFTERNOON -> SUNSET;
 			case SUNSET -> NIGHT;
-			case NIGHT -> SERVER;
+			case NIGHT -> MIDNIGHT;
+			case MIDNIGHT -> SERVER;
 			default -> SERVER;
 		};
 	}
@@ -31,11 +35,13 @@ public final class TimeType {
 	public static String from(int time) {
 		return switch (time) {
 			case SERVER -> "Server";
+			case SUNRISE -> "Sunrise";
 			case MORNING -> "Morning";
 			case NOON -> "Noon";
 			case AFTERNOON -> "Afternoon";
 			case SUNSET -> "Sunset";
 			case NIGHT -> "Night";
+			case MIDNIGHT -> "Midnight";
 			default -> "Server";
 		};
 	}
